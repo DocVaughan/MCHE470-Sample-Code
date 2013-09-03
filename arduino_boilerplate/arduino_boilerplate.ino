8f757429faaf510df7d33fd2874c8cb62126443f
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2013-09-02 19:42:02 -0500

    Initial Commit

---
+++ arduino_boilerplate/arduino_boilerplate.ino
@@ -0,0 +1,29 @@
+/*------------------------------------------------------------------------------------
+filename.ino
+
+Some description of functionality
+
+Optional Link to relativant documentation
+
+Created: mm/dd/yy - Name - email@louisiana.edu
+
+Modified:
+  * mm/dd/yy - Name (email if not same person as above)
+    - major change 1
+    - major change 2
+  * mm/dd/yy - Name (email if not same person as above)
+    - major change 1
+------------------------------------------------------------------------------------*/
+
+// This is always run once when the sketch starts
+// Use to initialize variables, pin modes, libraries, communication, etc
+void setup()
+{
+  // Your setup code goes here
+}
+
+// This runs immediately after setup, looping indefinitely
+void loop()
+{
+  // Your main code goes here
+}
