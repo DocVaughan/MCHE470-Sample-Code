0ff23513ede35b4952f58cee6e8da73b3d1f6882
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2013-09-03 01:05:58 -0500

    Updated Arduino boilerplate
    Updated Arduino boilerplate to include variable location explanation.

--- arduino_boilerplate/arduino_boilerplate.ino
+++ arduino_boilerplate/arduino_boilerplate.ino
@@ -3,7 +3,7 @@ filename.ino
 
 Some description of functionality
 
-Optional Link to relativant documentation
+Optional Link to relevant documentation
 
 Created: mm/dd/yy - Name - email@louisiana.edu
 
@@ -15,15 +15,25 @@ Modified:
     - major change 1
 ------------------------------------------------------------------------------------*/
 
+// Variables declared here are global
+int globalVariable;    // example declaration, globalVariable will be availble to all
+
 // This is always run once when the sketch starts
 // Use to initialize variables, pin modes, libraries, communication, etc
 void setup()
 {
+  // variables declared inside a function are only available there
+  byte setupVariableOnly;    // This byte variable is only available in steup()
+  
   // Your setup code goes here
 }
+
 
 // This runs immediately after setup, looping indefinitely
 void loop()
 {
+  // variables declared inside a function are only available there
+ boolean loopVariableOnly;    // This boolean variable is only available in loop()
+  
   // Your main code goes here
 }
